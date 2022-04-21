caefca901f12d46eab16cd7c7945eb7411092b66
Thierry Gonard <goth@octo.com>
2022-04-21 10:43:28 +0200

    taxe par pays

--- src/calc.ts
+++ src/calc.ts
@@ -7,15 +7,16 @@ export function sayHello() {
 export function calculeFacture(body: any): { total: number } {
   const total = accumulePrixEtQtt(body);
 
-  const totalAvecTaxes = appliquerTaxe(total);
+  const totalAvecTaxes = appliquerTaxeDePays(total, body.country);
 
   const totalAvecRéduction = appliquerReductionStandard(totalAvecTaxes);
 
   return { total: totalAvecRéduction };
 }
 
-function appliquerTaxe(total: number) {
-  return total * 1.2;
+function appliquerTaxeDePays(total: number, pays: string): number {
+  const taxe = taxeParPays[pays];
+  return total * taxe;
 }
 
 function appliquerReductionStandard(totalAprèsTaxes: number): number {
@@ -41,3 +42,34 @@ function accumulePrixEtQtt(body: any) {
   });
   return total;
 }
+
+const taxeParPays: Record<string, number> = {
+  DE: 1.2,
+  UK: 1.21,
+  FR: 1.2,
+  IT: 1.25,
+  ES: 1.19,
+  PL: 1.21,
+  RO: 1.2,
+  NL: 1.2,
+  BE: 1.24,
+  EL: 1.2,
+  CZ: 1.19,
+  PT: 1.23,
+  HU: 1.27,
+  SE: 1.23,
+  AT: 1.22,
+  BG: 1.21,
+  DK: 1.21,
+  FI: 1.17,
+  SK: 1.18,
+  IE: 1.21,
+  HR: 1.23,
+  LT: 1.23,
+  SI: 1.24,
+  LV: 1.2,
+  EE: 1.22,
+  CY: 1.21,
+  LU: 1.25,
+  MT: 1.2,
+};
